82278eb003912cf74587c09aed90824f65ba2e61
Harini Pavithra <harinipavithra0998@gmail.com>
2022-07-08 22:50:59 +0530

    Print Date

---
+++ Print Date.c
@@ -0,0 +1,16 @@
+A series of dates is passed as input. 
+The dates are in the format of day/month/year The program must print the historically largest date. 
+Ignore the invalid dates. If all the dates are invalid print -1.
+Example Input/Output 1:
+Input:
+2/4/2000 4/11/2018 22/12/2018 24/12/2018 22/12/1996
+
+Output:
+24/12/2018
+
+Example Input/Output 2:
+Input:
+29/14/1997 32/7/1996 15/8/1990 24/1/1999
+
+Output:
+24/01/1999
