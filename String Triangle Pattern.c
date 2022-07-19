d4ae90933c9ab5f658a0298d645d45c5c35fb1a5
Harini Pavithra <harinipavithra0998@gmail.com>
2022-07-19 13:58:47 +0530

    String Triangle Pattern

---
+++ String Triangle Pattern.c
@@ -0,0 +1,22 @@
+Example Input/Output 1: 
+Input: 
+3 
+apple mango orange 
+Output: 
+ple 
+ngo nge 
+Example Input/Output 2: 
+Input: 
+2 
+ask happy word letter king 
+Output: 
+sk 
+py rd 
+er ng ** 
+Explanation: The last 2 characters of the first word in S is "sk". 
+The first line of the output must contain one word. So, "sk" is printed. 
+The last 2 characters of the second and third word are "py" and "rd". 
+The second line of the output must contain two words separated by space(s). 
+So, "py" and "rd" printed. The last 2 characters of the fourth and fifth word are "er" and "ng". 
+The third line of the output must contain three words separated by space(s). But here, there are two words only.
+So, "er" and "ng" is printed. Then, for the third word "**" is printed.
