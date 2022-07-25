7d9c90421817621a92e460de869f5506d9f093e2
Harini Pavithra <harinipavithra0998@gmail.com>
2022-07-25 15:12:30 +0530

    T-String Pattern

---
+++ T-String Pattern.c
@@ -0,0 +1,9 @@
+#include&lt;stdio.h&gt;
+#include &lt;stdlib.h&gt;
+
+int main()
+{
+char s1[101],s2[101];
+scanf(&quot;%s %s&quot;,s1,s2);
+int i,l1=strlen(s1),l2=strlen(s2),r,c,p;
+printf(&quot;%s\n&quot;,s1);
